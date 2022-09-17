e560ada959fba4a5c48902ba4c2d439182a2e4ad
Rocket <me@rocketprogrammer.me>
2022-09-17 10:52:54 -0500

    toontown/pets: Fix proxy crash

--- toontown/pets/DistributedPetProxy.py
+++ toontown/pets/DistributedPetProxy.py
@@ -3,7 +3,6 @@ from direct.directnotify import DirectNotifyGlobal
 from toontown.pets import PetTraits
 from toontown.pets import PetMood, PetTricks
 from toontown.toonbase import ToontownGlobals
-import string
 
 class DistributedPetProxy(DistributedObject.DistributedObject):
     notify = DirectNotifyGlobal.directNotify.newCategory('DistributedPetProxy')
@@ -25,7 +24,7 @@ class DistributedPetProxy(DistributedObject.DistributedObject):
         self.requiredMoodComponents = {}
 
     def getSetterName(self, valueName, prefix = 'set'):
-        return '%s%s%s' % (prefix, string.upper(valueName[0]), valueName[1:])
+        return '%s%s%s' % (prefix, valueName[0].upper(), valueName[1:])
 
     def setOwnerId(self, ownerId):
         self.ownerId = ownerId
