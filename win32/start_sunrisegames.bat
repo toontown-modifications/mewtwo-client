72a7c2659cb630b3b1c25b59d0987daad7b0d908
2Epik4u <justinabossmlg@gmail.com>
2024-02-24 22:18:30 -0500

    general: update startup scripts

--- win32/start_sunrisegames.bat
+++ win32/start_sunrisegames.bat
@@ -2,10 +2,14 @@
 title Open Toontown - Game Client
 cd..
 
+echo Warning!
+echo This is for the Sunrise Games project.
+echo Make an account here https://toontastic.sunrise.games/
+
 rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
 set /P PPYTHON_PATH=<PPYTHON_PATH
 
-cls
+
 
 :main
 %PPYTHON_PATH% -m launcher
