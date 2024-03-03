dcd821c0d7ea54ec9d63098ea23c2cf9725ffe5c
2Epik4u <justinabossmlg@gmail.com>
2024-03-03 15:27:35 -0500

    Revert "shticker: no want to spam sunrise pls"
    This reverts commit 38562f43d8ee2b1fcebd6b20b94f3c28cd90e696.

--- toontown/shtiker/DirectNewsFrame.py
+++ toontown/shtiker/DirectNewsFrame.py
@@ -368,7 +368,7 @@ class DirectNewsFrame(DirectObject.DirectObject):
                 self.redownloadNews()
 
     def getInGameNewsUrl(self):
-        result = ConfigVariableString('fallback-news-url', 'http://cdn.toontown.disney.go.com/toontown/en/gamenews/').value
+        result = ConfigVariableString('fallback-news-url', 'http://download.sunrise.games/toontown/en/gamenews/').value
         override = ConfigVariableString('in-game-news-url', '').value
         if override:
             self.notify.info('got an override url,  using %s for in game news' % override)
