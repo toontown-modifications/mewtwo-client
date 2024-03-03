9cfdd13c1865b0d62f35aec24bcb5ceeebba457f
2Epik4u <justinabossmlg@gmail.com>
2024-03-03 15:25:04 -0500

    friends: fix a crash

--- toontown/friends/FriendInviter.py
+++ toontown/friends/FriendInviter.py
@@ -47,6 +47,8 @@ class FriendInviter(DirectFrame):
     def __init__(self, avId, avName, avDisableName):
         self.wantPlayerFriends = base.config.GetBool('want-player-friends', 0)
         DirectFrame.__init__(self, pos=(-1.033, 0.1, -0.35), parent=base.a2dTopRight, image_color=GlobalDialogColor, image_scale=(1.0, 1.0, 0.6), text='', text_wordwrap=TTLocalizer.FIdirectFrameWordwrap, text_scale=TTLocalizer.FIdirectFrame, text_pos=TTLocalizer.FIdirectFramePos)
+        self['image'] = DGG.getDefaultDialogGeom()
+        self.avId = avId
         self.toonName = avName
         avatar = base.cr.doId2do.get(self.avId)
         self.playerId = None
